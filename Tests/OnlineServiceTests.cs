2aa01b7ebb2058ae4a4016564b95535b59cf3dd3
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-06-16 16:28:04 +0200

    Fixing System.TypeLoadException

---
+++ Tests/OnlineServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Nefarius.Drivers.HidHide;
+using Nefarius.Vicius.Abstractions.Models;
+
+namespace Tests;
+
+internal partial class Tests
+{
+    [Test]
+    public async Task TestGetLatestReleaseAsync()
+    {
+        ServiceCollection sc = new();
+        sc.AddHidHide();
+
+        ServiceProvider sp = sc.BuildServiceProvider();
+
+        HidHideSetupProvider provider = sp.GetRequiredService<HidHideSetupProvider>();
+
+        UpdateRelease release = await provider.GetLatestReleaseAsync();
+        
+        Assert.Pass();
+    }
+}
