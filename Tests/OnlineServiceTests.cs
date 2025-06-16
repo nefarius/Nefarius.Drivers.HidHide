140e89d184ebf0fe0d6f4d9efe9adba8ac0fc076
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-06-16 17:40:50 +0200

    Added tests for HidHideSetupProvider
    Fixed TFM compatibility issues with vicius abstractions dependency

--- Tests/OnlineServiceTests.cs
+++ Tests/OnlineServiceTests.cs
@@ -18,7 +18,41 @@ internal partial class Tests
         HidHideSetupProvider provider = sp.GetRequiredService<HidHideSetupProvider>();
 
         UpdateRelease release = await provider.GetLatestReleaseAsync();
+
+        Assert.That(release, Is.Not.Null);
+    }
+
+    [Test]
+    public async Task TestGetLatestVersionAsync()
+    {
+        ServiceCollection sc = new();
+        sc.AddHidHide();
+
+        ServiceProvider sp = sc.BuildServiceProvider();
+
+        HidHideSetupProvider provider = sp.GetRequiredService<HidHideSetupProvider>();
+
+        Version version = await provider.GetLatestVersionAsync();
+
+        Assert.That(version, Is.EqualTo(Version.Parse("1.5.230.0")));
+    }
+
+    [Test]
+    public async Task TestDownloadLatestReleaseAsync()
+    {
+        ServiceCollection sc = new();
+        sc.AddHidHide();
+
+        ServiceProvider sp = sc.BuildServiceProvider();
+
+        HidHideSetupProvider provider = sp.GetRequiredService<HidHideSetupProvider>();
+
+        HttpResponseMessage response = await provider.DownloadLatestReleaseAsync();
+
+        Assert.That(response.IsSuccessStatusCode, Is.True);
+
+        byte[] body = await response.Content.ReadAsByteArrayAsync();
         
-        Assert.Pass();
+        Assert.That(body, Is.Not.Empty);
     }
 }
